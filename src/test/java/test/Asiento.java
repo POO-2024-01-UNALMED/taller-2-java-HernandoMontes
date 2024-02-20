0dcde6e9461109b1d9507b42589f1056a3828168
HernandoMontes <hmontesg@unal.edu.co>
2024-02-19 19:02:21 -0500

    intento5

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -4,7 +4,7 @@ public class Asiento{
 	int precio;
 	int registro;
 	
-	void cambiaColor (String color) {
+	void cambiarColor (String color) {
 		
 	if (color=="rojo" || color=="verde" || color=="amarillo" || color=="negro" || color=="blanco") {
 			this.color=color;
